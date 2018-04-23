0d93acb7e21a504db7219156fc38163853155ade
Chatchai Saratakij <CSaratakij@gmail.com>
2018-04-24 06:35:16 +0700

    Added BG music.

---
+++ RTS_TBS/Assets/Scripts/Utils/MarkSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LD41
+{
+    public class MarkSingleton : MonoBehaviour
+    {
+        public static GameObject instance = null;
+
+        void Awake()
+        {
+            if (instance == null) {
+                instance = gameObject;
+                DontDestroyOnLoad(gameObject);
+            }
+            else {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
